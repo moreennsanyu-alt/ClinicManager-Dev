653fa1f6c51c93127f4157e186f38a2862894b71
moreennsanyu-alt <moreenn.sanyu@gmail.com>
2026-06-21 23:23:55 +0300

    Last Sync: 2026-06-21 23:23 (Mobile)

---
+++ Src/Desktop.Tests/ClinicManager.Win.Tests/Sample.cs
@@ -0,0 +1,26 @@
+namespace nunit_runner_dotnet_test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+    [Test]
+    public void Test2()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test3()
+    {
+        Assert.Pass();
+    }
+}
